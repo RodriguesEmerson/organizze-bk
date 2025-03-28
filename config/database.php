2660529ff73861ce9a90ef869377c8b890a18eb2
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-03-28 19:43:32 -0300

    Files Structure bilt. Database connection done

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php 
+   require_once __DIR__ . '/loadEnv.php';
+   class Database{
+      private static $pdo;
+
+      public function getConnection(){
+         if(!self::$pdo){ //It checks if has already a connection
+
+            $db = getenv('DB_NAME');
+            $host = getenv('DB_HOST');
+            $user = getenv('DB_USER');
+            $password = getenv('DB_PASS');
+
+            try{
+
+               self::$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", "$user", "$password");
+               self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+               header('Content-Type: application/json');
+               echo json_encode(['message' => 'It was not possible conneting to the database.']);
+               die();
+            }
+         }
+         var_dump(self::$pdo);
+         return self::$pdo;
+      }
+   }
+?>
---
+++ config/loadEnv.php
@@ -0,0 +1,19 @@
+<?php 
+   function loadEnv($file = __DIR__ . '/../.env'){
+      if(!file_exists($file)){
+         die('Erro: File .env not found. Way:' . realpath($file));
+      }
+
+      $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //It ignores empty lines;
+
+      foreach($lines AS $line){
+         if(strpos(trim($line), '#') === 0) continue; //It ignores comments;
+
+         list($key, $value) = explode('=', $line, 2);
+         putenv("$key=$value");
+         $_ENV[$key] = $value;
+         $_SERVER[$key] = $value;
+      }
+   }
+   loadEnv();
+?>
